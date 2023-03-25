fc83435b35de3064b28767a0b101a50846154a08
yywyaoyaowu <yywyaoyaowu@gmail.com>
2023-03-25 17:11:16 +0800

    stat assembly

--- 01_assembly/01_hifiasm_purge.sh
+++ 01_assembly/01_hifiasm_purge.sh
@@ -22,20 +22,5 @@ seq_n50.pl ${prefix}_hifiasm.r_utg.fasta > ${prefix}_hifiasm.r_utg.fasta.n50
 seq_n50.pl ${prefix}_hifiasm.p_utg.fasta > ${prefix}_hifiasm.p_utg.fasta.n50
 seq_n50.pl ${prefix}_hifiasm.p_ctg.fasta > ${prefix}_hifiasm.p_ctg.fasta.n50
 
-#################### step3 purge_dups #####################
-minimap2 -t 16 -x map-pb ${prefix}.hifiasm.p_ctg.fasta ${prefix}.ccs.fastq > ${prefix}.hifiasm.p_ctg_aln.paf
-pbcstat ${prefix}.hifiasm.p_ctg_aln.paf
-calcuts PB.stat > cutoffs
-split_fa ${prefix}.hifiasm.p_ctg.fasta > ${prefix}.hifiasm.p_ctg_split.fa
-minimap2 -xasm5 -DP -t 16 ${prefix}.hifiasm.p_ctg_split.fa ${prefix}.hifiasm.p_ctg_split.fa > ${prefix}.hifiasm.p_ctg_split_self_aln.paf
-purge_dups -2 -T cutoffs -c PB.base.cov ${prefix}.hifiasm.p_ctg_split_self_aln.paf > dups.bed
-get_seqs -p ${prefix}.hifiasm.p_ctg_hifiasm dups.bed ${prefix}.hifiasm.p_ctg.fasta
-
-#purged.fa + hap.fa a_ctg.fa + homolog.fa;
-cat ${prefix}.hifiasm.p_ctg_hifiasm.purged.fa ${prefix}.hifiasm.p_ctg_hifiasm.hap.fa  ${prefix}.hifiasm.a_ctg.fasta > ${prefix}.hifiasm.p_ctg_final.fa
-
-#step4- KAT check (https://github.com/TGAC/KAT)
+#step3- KAT check (https://github.com/TGAC/KAT)
 kat comp -o p_ctg -t 30 -H 10000000000 -I 10000000000 -m 31 -h ${prefix}.ccs.fasta.gz ${prefix}_hifiasm.p_ctg.fasta
-kat comp -o purged -t 30 -H 10000000000 -I 10000000000 -m 31 -h ${prefix}.ccs.fasta.gz purged.fa
-kat comp -o final -t 30 -H 10000000000 -I 10000000000 -m 31 -h ${prefix}.ccs.fasta.gz final.fa
-
