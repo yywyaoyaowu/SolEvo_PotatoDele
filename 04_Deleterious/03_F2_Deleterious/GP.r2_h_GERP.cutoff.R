b57831b9b4147ad5c51c780bac872ac1e1b36173
yywyaoyaowu <74805920+yywyaoyaowu@users.noreply.github.com>
2023-05-17 17:12:28 +0800

    Update GP.r2_h_GERP.cutoff.R

--- 04_Deleterious/03_F2_Deleterious/GP.r2_h_GERP.cutoff.R
+++ 04_Deleterious/03_F2_Deleterious/GP.r2_h_GERP.cutoff.R
@@ -2,9 +2,6 @@
 # Objective : TODO
 # Created by: wuyaoyao
 # Created on: 2022/7/23
-setwd("/Users/wuyaoyao/work/03-evolutionary-dele/06_Sol_evo/03_Results/Paper/Output/NewDeleCutOff/F2/")
-#Model_h=read.table("GP_fixEffect_parameter_Stat.txt.final.txt",header=T)
-#GP_r=read.table("GP_differntGERP.CutOff_cross.validation.mean.final.txt",header=T)
 Model_h=read.table("GP_fixEffect_parameter_Stat_seq2.3.5_0.2.txt",header=T)
 Model_h$h=Model_h$L.Coefficients_Heter_rmOutlier/Model_h$L.Coefficients_Homo_rmOutlier
 library(ggplot2)
